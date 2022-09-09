7242a99507de2be888eb961e0a904174d217732a
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2022-09-09 14:47:27 +0530

    Create palindrome_smilpfied.js

---
+++ palindrome_smilpfied.js
@@ -0,0 +1,18 @@
+let substringPalindrome =(str,l,r)=>
+{
+    str = str.slice(l,r);//this will return the substring
+   
+
+    if(str==str,split("").reverse().join(""))
+    {
+        console.log("Palindrom");
+
+    }
+    else
+    {
+        console.log("not Palindrome")
+    }
+}
+let str ="aaabbaaa";
+substringPalindrome(srt,1,4)
+substringPalindrome(str,2,6)
